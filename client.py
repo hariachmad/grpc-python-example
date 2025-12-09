fb522dc5f67f9faaf2c188dbeb2f264df5258151
hariachmad <hari08achmad@gmail.com>
2025-12-09 10:00:35 +0700

    use ffmpeg

--- client.py
+++ client.py
@@ -3,34 +3,26 @@ import grpc
 import Audio_pb2
 import Audio_pb2_grpc
 import wave
-import opuslib
+import ffmpeg
 
 
 def send_audio():
     channel = grpc.insecure_channel("localhost:50051")
     stub = Audio_pb2_grpc.AudioServiceStub(channel)
     path =  "./recorded.wav"
-    channels = None
-    sample_rate = None
-    opus_frames = []
-    frame_size = 960
+    input_file = "recorded.wav"
+    output_file = "output.opus"
+    opus_frames = None
 
-    with wave.open(path, "rb") as wf:
-        channels = wf.getnchannels()
-        sample_rate = wf.getframerate()
-        encoder = opuslib.Encoder(
-        Fs=sample_rate,
-        channels=channels,
-        application=opuslib.APPLICATION_AUDIO)
-        opus_frames = []
-        while True:
-            pcm = wf.readframes(frame_size)
-            if not pcm:
-                break
+    (
+    ffmpeg
+    .input(input_file)
+    .output(output_file, acodec='libopus')
+    .run()
+    )
 
-            # Encode PCM → Opus frame
-            encoded = encoder.encode(pcm, frame_size)
-            opus_frames.append(encoded)
+    with open(output_file, "rb") as f:
+        opus_frames = f.read()
 
     request = Audio_pb2.AudioRequest(
         opus_data=opus_frames,
--- server.py
+++ server.py
@@ -3,11 +3,11 @@ from concurrent import futures
 
 import Audio_pb2
 import Audio_pb2_grpc
+import subprocess
 
 
 class AudioService(Audio_pb2_grpc.AudioServiceServicer):
     def SendAudio(self, request, context):
-        # Ambil data dari RPC
         opus_data = request.opus_data
         sample_rate = request.sample_rate
         channels = request.channels
@@ -15,6 +15,17 @@ class AudioService(Audio_pb2_grpc.AudioServiceServicer):
         print("Received Opus:", len(opus_data), "bytes")
         print("Sample Rate:", sample_rate)
         print("Channels:", channels)
+        
+        with open("temp.opus", "wb") as f:
+            f.write(opus_data)
+        
+        subprocess.run([
+            "ffmpeg", "-y", "-i", "temp.opus",
+            "-ar", str(sample_rate), "-ac", str(channels),
+            "output_server.wav"
+        ], check=True)
+
+        print("File WAV berhasil dibuat: output.wav")
 
         return Audio_pb2.AudioResponse(status="received")
 
